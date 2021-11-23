c5685510729d96857ff011fb7f25beb1f42225f2
LindaHashimoto <38723130+LindaHashimoto@users.noreply.github.com>
2021-11-23 15:54:25 -0300

    Add files via upload

--- ekadashi_script_02.js
+++ ekadashi_script_02.js
@@ -1,12 +1,12 @@
 var alimento = "";
 var dados = "";
-const pega_json = JSON.parse('{"alimentos_permitidos":["batata inglesa", "batata baroa", "batata doce", "inhame", "mandioca", "abobora", "pepino", "rabanete", "limao", "abacate", "azeitonas", "coco"], "alimentos_proibidos": ["pimentao", "tomate", "soja"]}');
+const pega_json = JSON.parse('{"alimentos_permitidos":["batata inglesa", "batata baroa", "batata doce", "inhame", "mandioca", "abobora", "pepino", "rabanete", "limao", "abacate", "azeitonas", "coco", "trigo mourisco", "oleo de coco", "azeite de oliva", "oleo de girassol","acucar mascavo", "acucar cristal nao refinado", "acucar demerara", "acucar de coco", "acucar de beterraba","leite fresco", "queijo sem coalho", "manteiga", "ghee", "gengibre fresco", "acafrao fresco", "pimenta do reino preta", "sal","pimenta preta", "tumerique fresco", "amendoim","coentro verde", "hortela verde", "salsinha verde", "curcuma"], "alimentos_proibidos": ["pimentao", "tomate", "soja", "arroz", "trigo", "cevada", "grao de bico", "ervilha","lentilha", "milho", "mostarda", "gergelim", "flocos de aveia", "flocos de arroz", "farinha de trigo", "farinha integral", "farinha de rosca", "po de arroz", "po de cereal", "oleo de soja", "oleo de milho", "oleo de gergelim", "amido de milho", "refrigerante", "sorvete", "semente de gergelim", "semente de mostarda", "semente de cominho", "cardamomo", "semente de papoula", "semente de kalonji", "semente de ajwain", "berinjela", "couve flor", "brocolis", "pimentao", "beterraba", "pepino amargo", "abobora branca", "cabaca", "toroi e kunli", "aspargo", "quiabo", "aipo", "salsao", "flor da bananeira", "champignon", "espinafre", "salsa", "coentro", "couve", "bicarbonato de sodio", "fermento em po"]}');
 
 const permitidos = pega_json.alimentos_permitidos;
 const proibidos = pega_json.alimentos_proibidos;
 
 function pega_valor(){
-	alimento = document.getElementById("busca_alimento").value;
+	alimento = (document.getElementById("busca_alimento").value).toLowerCase();
 	var pode = "";
 	for(var i = 0; i < permitidos.length;i++){
 		for(var j = 0; j < proibidos.length; j++){
@@ -36,8 +36,8 @@ function adiciona_item(){
 }
 
 /*For e if estão funcionando. Próximas alterações:
+
 melhorar o css
-Upar no github
 Fazer isto em React e React Native
 Upar a versão React e React Native no github
 Criar demo
